faf645904d3572c044b17b57d254a534138e4d07
Muhar Dwi Purwanto <k@muhar.us>
2019-11-23 23:11:05 +0700

    FIX: layout and page

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,12 @@
-<template>
-  <div class="container">
-    bertamu harus sopan
-  </div>
+<template lang="pug">
+  .container
+    h1
+      span.mustart-gradient-leaves-text Bertamu
+      span.mustart-gradient-leaves-text Harus
+      span.mustart-gradient-leaves-text Sopan
+      a(href="https://berta.muhar.us/sopan") https://berta.muhar.us/sopan
 </template>
 
 <script>
 export default {}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
