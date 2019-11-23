82720b743ce8e15f1a82823eccc77478fbd75610
Muhar Dwi Purwanto <k@muhar.us>
2019-11-23 23:13:06 +0700

    FIX: sopan update

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,7 @@
-<template lang="pug">
-  .container
-    h1
-      span.mustart-gradient-leaves-text Bertamu
-      span.mustart-gradient-leaves-text Harus
-      span.mustart-gradient-leaves-text Sopan
-      a(href="https://berta.muhar.us/sopan") https://berta.muhar.us/sopan
-</template>
-
 <script>
-export default {}
+export default {
+  mounted() {
+    this.$router.push('/sopan')
+  }
+}
 </script>
---
+++ pages/sopan.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .container
+    h1
+      span.mustart-gradient-leaves-text Bertamu
+      span.mustart-gradient-leaves-text Harus
+      span.mustart-gradient-leaves-text Sopan
+      a(href="https://berta.muhar.us/sopan") https://berta.muhar.us/sopan
+</template>
+
+<script>
+export default {}
+</script>
